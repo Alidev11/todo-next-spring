0dbf58cb060b0904ba582a96b0d59bed6a6cb3ed
Alidev11 <karwawiali@gmail.com>
2024-12-08 22:55:17 +0100

    fetch data success

--- frontend/src/app/tasks/addtask/page.tsx
+++ frontend/src/app/tasks/addtask/page.tsx
@@ -20,9 +20,9 @@ export default function CrudTasks() {
     return (
         <div className="">
             <NavBar></NavBar>
-            <div className="flex text-right justify-items-right justify-content-right align-items-right">
-                <button type="button" className="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 focus:outline-none dark:focus:ring-green-800">
-                    <p className="">Sign in</p>
+            <div className="flex">
+                <button type="button" className=" text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 focus:outline-none dark:focus:ring-green-800">
+                    <p className="capitalize">Add task</p>
                 </button>
             </div>
             <div className="rounded-lg relative overflow-x-auto px-6 py-6">
@@ -37,32 +37,18 @@ export default function CrudTasks() {
                     </thead>
                     <tbody>
                         {tasks.map((list, index) => (
-
                             <tr className="odd:bg-white even:bg-gray-50 even:dark:bg-gray-800 border-b odd:dark:bg-gray-900 dark:border-gray-700">
                                 <th scope="row" className="px-6 py-4">{list.id}</th>
                                 <th scope="row" className="px-6 py-4">{list.title}</th>
                                 <th scope="row" className="px-6 py-4">{list.description}</th>
                                 <th scope="row" className="px-6 py-4">
-                                    <div className="form-check">
-                                        <input className="form-check-input" type="checkbox" value="" id="" checked={list.description} />
-                                        <label className="form-check-label" for="">Completed </label>
+                                    <div className="">
+                                        <input className="form-check-input" type="checkbox" value="" id="completed" name="completed" checked={list.description} />
+                                        <label className="form-check-label" htmlFor="completed">Completed </label>
                                     </div>
                                 </th>
                             </tr>
                         ))}
-
-                        {/* <tr className="odd:bg-white even:bg-gray-50 even:dark:bg-gray-800 border-b odd:dark:bg-gray-900 dark:border-gray-700">
-                            <th scope="row" className="px-6 py-4">1</th>
-                            <th scope="row" className="px-6 py-4">Task1</th>
-                            <th scope="row" className="px-6 py-4">this task is a task Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odit nemo aperiam minima quaerat autem qui illum nesciunt neque id adipisci impedit, molestiae possimus eaque non culpa repellendus deleniti odio quo.</th>
-                            <th scope="row" className="px-6 py-4">Completed</th>
-                        </tr>
-                        <tr className="odd:bg-white even:bg-gray-50 even:dark:bg-gray-800 border-b odd:dark:bg-gray-900 dark:border-gray-700">
-                            <th scope="row" className="px-6 py-4">1</th>
-                            <th scope="row" className="px-6 py-4">Task1</th>
-                            <th scope="row" className="px-6 py-4">this task is a task Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odit nemo aperiam minima quaerat autem qui illum nesciunt neque id adipisci impedit, molestiae possimus eaque non culpa repellendus deleniti odio quo.</th>
-                            <th scope="row" className="px-6 py-4">Completed</th>
-                        </tr> */}
                     </tbody>
                 </table>
             </div>
